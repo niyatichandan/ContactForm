7b4391d1561a3fd410e7855e2c9044c6a3469ac3
Niyati <niyatichandan@gmail.com>
2021-07-10 20:09:40 +0530

    Updated contact form functionality - validations, DB changes, added CSS

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,6 +1,27 @@
 class ContactsController < ApplicationController
+  protect_from_forgery
+
 	def new
     I18n.locale = params[:locale]
     @contact = Contact.new
+  end
+
+  #  Create new contact
+  def create
+    @contact = Contact.new(contact_params.merge(locale: I18n.locale))
+    respond_to do |format|
+      if @contact.save
+        format.html { redirect_to root_path, notice: I18n.t('contact.created') }
+      else
+        format.html { render 'contacts/new' }
+      end
+    end
+  end
+
+  private
+
+  # Only allow a list of trusted parameters through.
+  def contact_params
+    params.permit('first_name', 'last_name', 'email', 'phone_number', 'message', 'locale')
   end
 end
